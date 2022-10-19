3bff04a62eb021cd4c63c386c2407f763d8afa22
NTN47 <88170793+NTN47@users.noreply.github.com>
2022-10-19 16:14:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,9 @@ const CONFIG = {
     Chúc em hạnh phúc
     mãi yêuu em ❤️"`,
     btnIntro: ' ',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Em có thể cho anh một cơ hội được không?',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Love you 3000 😘😘',
-    messDesc: 'Hà Như Uyên.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/trung.nguyen.178/'
-}
+    title: 'Chúng ta còn có thể bên nhau? 😙'
+    btnYes: 'có thể',
+    btnNo: 'không thể',
+    question:'Lí do tại sao ?',
+    btnReply: 'Trả lời anh',
+    
